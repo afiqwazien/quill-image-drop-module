9ae1b95dda356ec6ac74d39ae844d7a41cbdd899
mohammad afiq wazien mohamad nawawi <afiqwazien.nawawi@gmail.com>
2019-11-04 15:43:19 +0800

    Update ImageDrop.js

--- src/ImageDrop.js
+++ src/ImageDrop.js
@@ -26,6 +26,7 @@ export class ImageDrop {
 	 * @param {Event} evt
 	 */
 	handleDrop(evt) {
+		console.log(evt);
 		evt.preventDefault();
 		if (evt.dataTransfer && evt.dataTransfer.files && evt.dataTransfer.files.length) {
 			if (document.caretRangeFromPoint) {
@@ -44,11 +45,15 @@ export class ImageDrop {
 	 * @param {Event} evt
 	 */
 	handlePaste(evt) {
+		console.log(evt);
 		if (evt.clipboardData && evt.clipboardData.items && evt.clipboardData.items.length) {
+			console.log(1);
 			for (const item of evt.clipboardData.items) {
+			console.log(2);
 				if (item.kind === 'file') {
 				evt.preventDefault();
 				this.readFiles(evt.clipboardData.items, dataUrl => {
+					console.log(3);
 					const selection = this.quill.getSelection();
 					this.insert(dataUrl);
 					// Move after the image
